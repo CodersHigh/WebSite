2fc1ee99c03698fdcbb23569a146120a53b8426c
sr9872@naver.com <sr9872@naver.com>
2019-08-12 12:41:48 +0900

    minor

--- assets/studentsData/studentsOutcomesForCards.js
+++ assets/studentsData/studentsOutcomesForCards.js
@@ -1,7 +1,7 @@
 var universityData = {
   "studentsDataForCards": [
     {
-      "appScreen": "../assets/img/report-appscreen/dgu_18_01/05.png",
+      "appScreen": "assets/img/report-appscreen/dgu_18_01/05.png",
       "appTitle": "원샷톡",
       "appComment": "여러 명에게 한번에 메시지를 전송하고 싶었던 사용자들을 위한 다수의 개인톡을 보낼 수 있도록 해주는 앱",
       "students": "박신원 박지용 이다훈",
@@ -10,7 +10,7 @@ var universityData = {
       "website": "https://projectintheclass.github.io/OneshotTalk/"
     },
     {
-      "appScreen": "../assets/img/report-appscreen/dgu_18_01/06.png",
+      "appScreen": "assets/img/report-appscreen/dgu_18_01/06.png",
       "appTitle": "PillingTime",
       "appComment": "약을 복용하는 사람들을 위한 시간을 등록하면 해당시간에 약을 복용할 수 있도록 알려주는 알람 앱",
       "students": "김승주 신유리 이해준",
@@ -19,7 +19,7 @@ var universityData = {
       "website": "https://projectintheclass.github.io/PillingTime/"
     },
     {
-      "appScreen": "../assets/img/report-appscreen/dgu_18_01/01.png",
+      "appScreen": "assets/img/report-appscreen/dgu_18_01/01.png",
       "appTitle": "도리니티",
       "appComment": "3D모델 교육을 위한 앱",
       "students": "김민수 김성탁 김지수",
@@ -28,7 +28,7 @@ var universityData = {
       "website": "https://projectintheclass.github.io/dorinity/"
     },
     {
-      "appScreen": "../assets/img/report-appscreen/swu_18_02/03.png",
+      "appScreen": "assets/img/report-appscreen/swu_18_02/03.png",
       "appTitle": "세이브 돈돈",
       "appComment": "돈돈이와 함께라면 당신도 즐거운 저축과 신나는 소비가 가능합니다!",
       "students": "배민주 홍정민",
@@ -37,7 +37,7 @@ var universityData = {
       "website": "https://projectintheclass.github.io/SaveDonDon"
     },
     {
-      "appScreen": "../assets/img/report-appscreen/swu_18_02/04.png",
+      "appScreen": "assets/img/report-appscreen/swu_18_02/04.png",
       "appTitle": "재고재고",
       "appComment": "식당의 효율적인 음식재고 관리와 일정관리",
       "students": "성다연 이유진 정유림",
@@ -46,7 +46,7 @@ var universityData = {
       "website": "https://projectintheclass.github.io/JaegoJaego"
     },
     {
-      "appScreen": "../assets/img/report-appscreen/cau_18_01/02.png",
+      "appScreen": "assets/img/report-appscreen/cau_18_01/02.png",
       "appTitle": "모으다",
       "appComment": "책을 좋아하는 사람들을 위한, 책 속 문장을 남길 수 있는 기능을 가진 앱",
       "students": "김수진 이예주",
@@ -55,7 +55,7 @@ var universityData = {
       "website": "https://projectintheclass.github.io/Mouda/"
     },
     {
-      "appScreen": "../assets/img/report-appscreen/cau_18_01/06.png",
+      "appScreen": "assets/img/report-appscreen/cau_18_01/06.png",
       "appTitle": "기숙사알리미",
       "appComment": "공지사항부터 생활정보까지, 모든 것을 한 눈에",
       "students": "안재형 한진 강제순",
@@ -64,7 +64,7 @@ var universityData = {
       "website": "https://projectintheclass.github.io/CAUDormitory/"
     },
     {
-      "appScreen": "../assets/img/report-appscreen/hyu_18_01/10.png",
+      "appScreen": "assets/img/report-appscreen/hyu_18_01/10.png",
       "appTitle": "스마트한양",
       "appComment": "한양대생들의 삶의 질 개선을 위한 시간표 프로젝트",
       "students": "백종원 유지원 현경우",
