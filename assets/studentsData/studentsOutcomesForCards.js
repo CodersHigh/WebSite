b227614122a9658f33c8380726b158c782701de5
sr9872@naver.com <sr9872@naver.com>
2019-08-12 12:31:47 +0900

    minor revised
    students reports

--- assets/studentsData/studentsOutcomesForCards.js
+++ assets/studentsData/studentsOutcomesForCards.js
@@ -1,18 +1,76 @@
 var universityData = {
   "studentsDataForCards": [
     {
-      "appScreen": "assets/img/report-appscreen/hyu_19_01/01.png",
-      "appTitle": "MinZzengE",
-      "appComment": "민쯩이는 당신이 지갑에서 신분증을 꺼낼 수고를 덜어줍니다.",
-      "students": "김정모, 김효일, 최성인",
-      "website": "https://projectintheclass.github.io/MinZzeungE/"
+      "appScreen": "../assets/img/report-appscreen/dgu_18_01/05.png",
+      "appTitle": "원샷톡",
+      "appComment": "여러 명에게 한번에 메시지를 전송하고 싶었던 사용자들을 위한 다수의 개인톡을 보낼 수 있도록 해주는 앱",
+      "students": "박신원 박지용 이다훈",
+      "youTubeAddress": "https://www.youtube.com/embed/pkltcxtA03w",
+      "appStoreAddress": "https://itunes.apple.com/us/app/%EC%9B%90%EC%83%B7%ED%86%A1/id1350334218?mt=8",
+      "website": "https://projectintheclass.github.io/OneshotTalk/"
     },
     {
-      "appScreen": "assets/img/report-appscreen/hyu_19_01/01.png",
-      "appTitle": "두번째",
-      "appComment": "민쯩이는 당신이 지갑에서 신분증을 꺼낼 수고를 덜어줍니다.",
-      "students": "김정모, 김효일, 최성인",
-      "website": "https://projectintheclass.github.io/MinZzeungE/"
-    }
+      "appScreen": "../assets/img/report-appscreen/dgu_18_01/06.png",
+      "appTitle": "PillingTime",
+      "appComment": "약을 복용하는 사람들을 위한 시간을 등록하면 해당시간에 약을 복용할 수 있도록 알려주는 알람 앱",
+      "students": "김승주 신유리 이해준",
+      "youTubeAddress": "https://www.youtube.com/embed/9hZ865UkV10",
+      "appStoreAddress": "https://itunes.apple.com/us/app/%ED%95%84%EB%A7%81%ED%83%80%EC%9E%84/id1350330770?mt=8",
+      "website": "https://projectintheclass.github.io/PillingTime/"
+    },
+    {
+      "appScreen": "../assets/img/report-appscreen/dgu_18_01/01.png",
+      "appTitle": "도리니티",
+      "appComment": "3D모델 교육을 위한 앱",
+      "students": "김민수 김성탁 김지수",
+      "youTubeAddress": "https://www.youtube.com/embed/1bBU9ReBk3E",
+      "appStoreAddress": "https://itunes.apple.com/us/app/%EB%8F%84%EB%A6%AC%EB%8B%88%ED%8B%B0/id1350340428?mt=8",
+      "website": "https://projectintheclass.github.io/dorinity/"
+    },
+    {
+      "appScreen": "../assets/img/report-appscreen/swu_18_02/03.png",
+      "appTitle": "세이브 돈돈",
+      "appComment": "돈돈이와 함께라면 당신도 즐거운 저축과 신나는 소비가 가능합니다!",
+      "students": "배민주 홍정민",
+      "youTubeAddress": "https://www.youtube.com/embed/SqEodyJWKwU",
+      "appStoreAddress": "https://itunes.apple.com/kr/app/id1423018646",
+      "website": "https://projectintheclass.github.io/SaveDonDon"
+    },
+    {
+      "appScreen": "../assets/img/report-appscreen/swu_18_02/04.png",
+      "appTitle": "재고재고",
+      "appComment": "식당의 효율적인 음식재고 관리와 일정관리",
+      "students": "성다연 이유진 정유림",
+      "youTubeAddress": "https://www.youtube.com/embed/GwaGQ8tBM1M",
+      "appStoreAddress": "https://itunes.apple.com/kr/app/%EC%9E%AC%EA%B3%A0%EC%9E%AC%EA%B3%A0/id1423132116?mt=8",
+      "website": "https://projectintheclass.github.io/JaegoJaego"
+    },
+    {
+      "appScreen": "../assets/img/report-appscreen/cau_18_01/02.png",
+      "appTitle": "모으다",
+      "appComment": "책을 좋아하는 사람들을 위한, 책 속 문장을 남길 수 있는 기능을 가진 앱",
+      "students": "김수진 이예주",
+      "youTubeAddress": "https://www.youtube.com/embed/V6rgTDeOfwI",
+      "appStoreAddress": "https://itunes.apple.com/kr/app/1%EC%9B%94-1%EC%9D%BC-%EB%AA%A9%ED%91%9C-%EA%B4%80%EB%A6%AC-%EC%96%B4%ED%94%8C%EB%A6%AC%EC%BC%80%EC%9D%B4%EC%85%98/id1218696438?mt=8",
+      "website": "https://projectintheclass.github.io/Mouda/"
+    },
+    {
+      "appScreen": "../assets/img/report-appscreen/cau_18_01/06.png",
+      "appTitle": "기숙사알리미",
+      "appComment": "공지사항부터 생활정보까지, 모든 것을 한 눈에",
+      "students": "안재형 한진 강제순",
+      "youTubeAddress": "https://www.youtube.com/embed/CmXXqvfjHbg",
+      "appStoreAddress": "https://itunes.apple.com/kr/app/%EC%A4%91%EC%95%99%EB%8C%80%EA%B8%B0%EC%88%99%EC%82%AC%EC%95%8C%EB%A6%AC%EB%AF%B8/id1363880842?mt=8",
+      "website": "https://projectintheclass.github.io/CAUDormitory/"
+    },
+    {
+      "appScreen": "../assets/img/report-appscreen/hyu_18_01/10.png",
+      "appTitle": "스마트한양",
+      "appComment": "한양대생들의 삶의 질 개선을 위한 시간표 프로젝트",
+      "students": "백종원 유지원 현경우",
+      "youTubeAddress": "https://www.youtube.com/embed/pk0O_N8N388?list=PLBjx8AiS7qA0rtzpNCfKu_e54Kmd-A50D",
+      "appStoreAddress": "https://itunes.apple.com/us/app/smarthanyang/id1420489675?ls=1&mt=8",
+      "website": "https://projectintheclass.github.io/SmartHanyang"
+    },
 ]
 }
