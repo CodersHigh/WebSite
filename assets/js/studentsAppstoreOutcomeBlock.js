9cd9367fcd54b2b5ec69543e099b71ecdb7a8c8b
sr9872@naver.com <sr9872@naver.com>
2019-08-23 16:40:29 +0900

    Student'sOutcome_Template_Added
    1. students'Outcome_Template_Added
    2. Plus && Close Button is being developed

--- assets/js/studentsAppstoreOutcomeBlock.js
+++ assets/js/studentsAppstoreOutcomeBlock.js
@@ -1,22 +1,66 @@
 <!--  아래는 수정 금지  -->
 
 Vue.component('student-outcome-appstore', {
-  props: ['university'],
+  props: ['data'],
   template: '\
   <div>\
-    <div class="container centered" style="margin-bottom: 80px;">\
-      <div class="col-md-4 col-xs-12" style="margin-bottom: 100px;" v-for="value in studentsOutcomes">\
-        <div class="card" style="margin-right: 50px;margin-left: 50px;">\
-          <img class="card-img-top" :src="value.appScreen">\
-          <div class="card-body">\
-            <h4 class="appTitle margin-none">{{value.appTitle}}</h4>\
-            <p class="appComment" style=" color: #949494">{{value.appComment}}</p>\
-            <p class="card-text">{{value.students}}</p>\
-            <a :href="value.website" target="_blank" class="btn btn-primary">자세히 보기</a>\
-          </div>\
+  <div class="row article" v-for="value in studentsOutcomes">\
+    <div class="row">\
+      <div class="col-md-2 col-xs-12 centered"  style=" border-style: solid; border-color: coral;">\
+        <img class="appIcon" :src="value.appIcon">\
+      </div>\
+      <div class="col-md-10 col-xs-12"  style=" border-style: solid; border-color: coral;">\
+        <div style="border-style: solid; border-color: coral;">\
+          <h3 class="appTitle margin-none">{{value.appTitle}}</h3>\
+        </div>\
+        <div>\
+          <p class="appComment">{{value.appComment}}</p>\
+        </div>\
+        <div>\
+          <h4>{{value.students}}</h4>\
+        </div>\
+        <div>\
+          <a :href="value.appStoreAddress" target="_blank">\
+            <img src="assets/img/app-store.png" style="padding: 5px 0.5px; height: 48px;">\
+          </a>\
+        </div>\
+        <div>\
+          <a :href="value.website" target="_blank" style="margin-right: 20px;">\
+            개발자 웹사이트 <img src="assets/img/report-appstore/01/arrow.svg" style="height: 12px;">\
+          </a>\
+          <a v-bind:src="value.youTubeAddress" target="_blank">\
+            발표 영상 <img src="assets/img/report-appstore/01/arrow.svg" style="height: 12px;">\
+          </a>\
         </div>\
       </div>\
     </div>\
+    <hr />\
+    <div style="border-style: solid; border-color: coral;">\
+      <h4 class="appTitle margin-none"> iPhone 스크린샷 </h4>\
+      <div class="row">\
+        <div class="col-md-3 col-xs-12">\
+          <img class="screenImage" :src="value.appScreen01" style="padding-bottom: 10px;">\
+        </div>\
+        <div class="col-md-3 col-xs-12">\
+          <img class="screenImage" :src="value.appScreen02" style="padding-bottom: 10px;">\
+        </div>\
+        <div class="col-md-3 col-xs-12">\
+          <img class="screenImage" :src="value.appScreen03" style="padding-bottom: 10px;">\
+        </div>\
+        <div class="col-md-3 col-xs-12">\
+          <img class="screenImage" :src="value.appScreen04" style="padding-bottom: 10px;">\
+        </div>\
+      </div>\
+    </div>\
+    <hr />\
+    <div style="border-style: solid; border-color: coral;">\
+      <h4 class="appTitle margin-none"> 관련링크 </h4>\
+      <a v-show="value.cardNewsAddress != null" :href="value.cardNewsAddress" target="_blank">\
+      <img :src="value.cardNewsImage" style="width: 200px; height: 200px">\
+      </a>\
+    </div>\
+  </div>\
+  <hr />\
   </div>\
   ',
   data() {
@@ -25,13 +69,13 @@ Vue.component('student-outcome-appstore', {
             }
       },
   mounted() {
-    this.studentsOutcomes = universityData[this.university];
+    this.studentsOutcomes = universityData[this.data];
   }
 })
 
 
 window.onload = function() {
     new Vue({
-      el: '#root1'
+      el: '#root'
     })
 }
--- assets/studentsData/studentsOutcomesForAppstore.js
+++ assets/studentsData/studentsOutcomesForAppstore.js
@@ -1,21 +1,34 @@
 var universityData = {
   "studentsOutcomesForAppstore": [
     {
-      "appScreen": "assets/img/report-appscreen/hyu_19_01/06.png",
+      "appIcon": "assets/img/report-appStore/01/appIcon.png",
       "appTitle": "AboutHanyang",
       "appComment": "AboutHanyang은 한양대 내의 시설 정보를 공유하고 의견을 나누는 앱입니다.",
       "students": "김태형, 이성주, 정재윤",
+      "appStoreAddress": "https://apps.apple.com/kr/app/about-hanyang/id1469853999",
       "youTubeAddress": "https://www.youtube.com/embed/51AC4AyospE",
-      "appStoreAddress": "https://apps.apple.com/kr/app/about-hanyang/id1469853999"
+      "website": "https://www.naver.com",
+      "appScreen01": "assets/img/report-appscreen/hyu_19_01/06.png",
+      "appScreen02": "assets/img/report-appscreen/hyu_19_01/06.png",
+      "appScreen03": "assets/img/report-appscreen/hyu_19_01/06.png",
+      "appScreen04": "assets/img/report-appscreen/hyu_19_01/06.png",
+      "cardNewsAddress": "https://www.naver.com",
+      "cardNewsImage": "assets/img/report-appStore/01/cardNews01.png"
     },
     {
-      "appScreen": "assets/img/report-appscreen/hyu_19_01/02.png",
-      "appTitle": "Hanyang Smart Order",
-      "appComment": "한양대학교 교내 카페 주문 결제 / 쿠폰 관리 어플리케이션",
-      "students": "전하영, 최준호, 정서운",
-      "youTubeAddress": "https://www.youtube.com/embed/F-LaJpYToKw",
-      "appStoreAddress": "https://apps.apple.com/kr/app/한양-스마트오더/id1470195139",
-      "website": "https://projectintheclass.github.io/QueueOrder/"
+      "appIcon": "assets/img/report-appStore/01/appIcon.png",
+      "appTitle": "AboutHanyang",
+      "appComment": "AboutHanyang은 한양대 내의 시설 정보를 공유하고 의견을 나누는 앱입니다.",
+      "students": "김태형, 이성주, 정재윤",
+      "appStoreAddress": "https://apps.apple.com/kr/app/about-hanyang/id1469853999",
+      "youTubeAddress": "https://www.youtube.com/embed/51AC4AyospE",
+      "website": "https://www.naver.com",
+      "appScreen01": "assets/img/report-appscreen/hyu_19_01/06.png",
+      "appScreen02": "assets/img/report-appscreen/hyu_19_01/06.png",
+      "appScreen03": "assets/img/report-appscreen/hyu_19_01/06.png",
+      "appScreen04": "assets/img/report-appscreen/hyu_19_01/06.png",
+      "cardNewsAddress": "https://www.naver.com",
+      "cardNewsImage": "assets/img/report-appStore/01/cardNews01.png"
     }
 ]
 }
