54f019493d33ded673fc750ad7f181dc0724520d
sr9872@naver.com <sr9872@naver.com>
2019-08-23 10:31:54 +0900

    first_commit_for_appstoreStyle

---
+++ assets/studentsData/studentsOutcomesForAppstore.js
@@ -0,0 +1,21 @@
+var universityData = {
+  "studentsOutcomesForAppstore": [
+    {
+      "appScreen": "assets/img/report-appscreen/hyu_19_01/06.png",
+      "appTitle": "AboutHanyang",
+      "appComment": "AboutHanyang은 한양대 내의 시설 정보를 공유하고 의견을 나누는 앱입니다.",
+      "students": "김태형, 이성주, 정재윤",
+      "youTubeAddress": "https://www.youtube.com/embed/51AC4AyospE",
+      "appStoreAddress": "https://apps.apple.com/kr/app/about-hanyang/id1469853999"
+    },
+    {
+      "appScreen": "assets/img/report-appscreen/hyu_19_01/02.png",
+      "appTitle": "Hanyang Smart Order",
+      "appComment": "한양대학교 교내 카페 주문 결제 / 쿠폰 관리 어플리케이션",
+      "students": "전하영, 최준호, 정서운",
+      "youTubeAddress": "https://www.youtube.com/embed/F-LaJpYToKw",
+      "appStoreAddress": "https://apps.apple.com/kr/app/한양-스마트오더/id1470195139",
+      "website": "https://projectintheclass.github.io/QueueOrder/"
+    }
+]
+}
